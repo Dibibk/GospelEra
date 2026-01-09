fc72672353ccaa73d04c136665b3d567521f714f
Dibibk <diviabharath@gmail.com>
2026-01-09 08:22:30 -0600

    changes related to new sender id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.gospelera.app',
-  appName: 'Gospel Era',
+  appName: 'Gospel Era App',
   webDir: 'dist/public',
 
   server: {
